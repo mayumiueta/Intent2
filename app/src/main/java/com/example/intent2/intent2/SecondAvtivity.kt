1378cc4ca992f91d08330c0f0dbc78dd6337e9d0
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-05-01 22:58:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intent2/intent2/SecondAvtivity.kt
@@ -0,0 +1,12 @@
+package com.example.intent2.intent2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondAvtivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
